871e29005eafda328ab0ce1901703d96152e4649
Alex Harston <j.harston17@imperial.ac.uk>
2018-05-07 01:02:49 +0100

    added post details

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('^$', views.post_list, name='post_list')
+    re_path('^$', views.post_list, name='post_list'),
+    re_path('^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
 ]
