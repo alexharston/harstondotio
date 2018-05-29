7098d9da5488ed13c4da645d87a98e451e5a81ec
Alex Harston <j.harston17@imperial.ac.uk>
2018-05-29 13:28:27 +0100

    spacing to 80 characters

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,8 @@ def home(request):
     return render(request, 'blog/home.html')
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+    'published_date').reverse()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
