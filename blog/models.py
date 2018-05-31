e4a02a7d37e5fec26d2265ac0a73d6da9600c8dd
Alex Harston <j.harston17@imperial.ac.uk>
2018-05-31 18:48:46 +0100

    small changes

--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,14 @@ class Post(models.Model):
     created_date = models.DateTimeField(default = timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
-    def __str__(self):
-        return self.title
+    
 
 class Paper(models.Model):
     title = models.CharField(max_length=300)
@@ -25,7 +27,6 @@ class Paper(models.Model):
     bodytext = models.TextField()
 
 class Poster(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     published_year = models.DateField(blank=True, null=True)
