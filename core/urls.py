6873f7166e61988cd63faa5cc31868351bede0c6
Alex Harston <alex@harston.io>
2020-09-06 03:21:29 +0100

    fixed post links

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('post/<slug:slug   >', views.post_detail),
+    path('writing/<slug:slug>', views.post_detail),
     path('projects/', views.projects),
     path("research/", views.research),
     path("design/", views.designs),
